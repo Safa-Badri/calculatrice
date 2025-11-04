82a245200c09b082e64b6724be172fd9fb2c84f7
safa.badri@enis.tn <safa.badri@enis.tn>
2025-11-04 21:15:19 +0100

    Ajout d’un message bienvenue

--- Operation.java
+++ Operation.java
@@ -1,6 +1,12 @@
 class Operation {
 
+public static int addition(int a, int b) {
+        return a + b;
+    }
 
+    public static int soustraction(int a, int b) {
+        return a - b;
+    }
 
     public static int multiplication(int a, int b) {
         return a * b;
