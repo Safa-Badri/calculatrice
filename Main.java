60bc9a14e8a811acd49391155c06d5adc905f4b4
safa.badri@enis.tn <safa.badri@enis.tn>
2025-11-04 21:55:22 +0100

    Rajout d’un test de la vérification de la valeur de y dans la division

--- Main.java
+++ Main.java
@@ -3,15 +3,24 @@ public class Main {
           int a = 2;
           int b = 3;
 
-System.out.println("Bienvenue");          
- double resultatDivision = Operation.division(a, b);
+System.out.println("Bienvenue");
+
+
  int resultat = Operation.multiplication(a, b);
  int resultatSoustraction = Operation.soustraction(a, b);
  int resultatAddition = Operation.addition(a, b);
 
+if (b == 0) {
+System.out.println("Erreur : Division par zéro interdite !");
+ } else {
+ double resultatDivision = Operation.division(a, b);
+   System.out.println(a + " / " + b + " = " + resultatDivision);
+}
+
+
 System.out.println(a + " * " + b + " = " + resultat);
 System.out.println(a + " - " + b + " = " + resultatSoustraction);
-System.out.println(a + " + " + b + " = " + resultatAddition); 
+System.out.println(a + " + " + b + " = " + resultatAddition);
 
       } 
 }
