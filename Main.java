4924bbede3a680f7c5030787b1f1c43ab4b9c22d
safa.badri@enis.tn <safa.badri@enis.tn>
2025-11-04 20:33:40 +0100

    Ajout de la soustraction et de l’addition et leurs tests avec a et b comme paramètres et affichage de la valeur de a, de la valeur de b et la valeur de l’opération de calcul effectuée

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+      public static void main(String[] args) {
+          int a = 2;
+          int b = 3;
+          int resultat = Operation.multiplication(a, b);
+          int resultatSoustraction = Operation.soustraction(a, b);
+          int resultatAddition = Operation.addition(a, b);
+
+        System.out.println(a + " * " + b + " = " + resultat);
+        System.out.println(a + " - " + b + " = " + resultatSoustraction);
+        System.out.println(a + " + " + b + " = " + resultatAddition); 
+      } 
+}
